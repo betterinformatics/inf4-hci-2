adc5a4760b57714003a3e77183678faef222701e
AngusP <a7cp3ar50n@gmail.com>
2016-11-13 18:30:36 +0000

    Adding scrollspy quick nav
    For large screens only...

--- js/scroll.js
+++ js/scroll.js
@@ -28,3 +28,51 @@ $(function() {
         return false; // Suppress default behaviour
     });
 });
+
+
+/* Timleine scrollspy */
+$(document).ready(function() {
+    $('#t1Affix').affix({
+        offset: {
+            top: 660,
+            bottom: function () {
+                var fttr = $('#footer');
+                var fttr_pos = fttr.height() + fttr.offset().top;
+                return (this.bottom = fttr_pos - $('#mapHero').offset().top + 200)
+            }
+        }
+    });
+
+    $('#t2Affix').affix({
+        offset: {
+            top: function () {
+                return (this.top = $('#mapHero').offset().top)
+            },
+            bottom: function () {
+                return (this.bottom = $('#footer').outerHeight(true))
+            }
+        }
+    });
+});
+
+/* Scroll around based on keyboard presses */
+
+$(document).keydown(function(e) {
+    switch(e.which) {
+    /* Scroll to previous card: */
+    case 37: // left
+    case 38: // up
+        // TODO!
+        break;
+
+    /* Scroll to next card:  */
+    case 39: // right
+    case 40: // down
+        // TODO!
+        break;
+
+    default: return; // exit this handler for other keys
+    }
+    e.preventDefault(); // prevent the default action (scroll / move caret)
+});
+
